505dc228936be9bcfa80d275ca1597f2fb98beda
LeonNjoroge <leonmuraguri@gmail.com>
2025-02-16 15:24:56 +0300

    Create Home Component

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgClass } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -11,18 +12,21 @@ export class NavbarComponent implements OnInit {
   activeTab: number | null= null;
   
   navItems= [
-    { name: 'HOME', activeImg: 'assets/icons/home.svg', inactiveImg: 'assets/icons/home-bl.svg' },
-    { name: 'CATEGORIES', activeImg: 'assets/icons/categories.svg', inactiveImg: 'assets/icons/categories-bl.svg' },
-    { name: 'MY CART', activeImg: 'assets/icons/cart.svg', inactiveImg: 'assets/icons/cart-bl.svg' },
-    { name: 'PAYMENTS', activeImg: 'assets/icons/payments.svg', inactiveImg: 'assets/icons/payments-bl.svg' },
-    { name: 'MORE', activeImg: 'assets/icons/more.svg', inactiveImg: 'assets/icons/more-bl.svg' }
+    { name: 'HOME', route: '/home', activeImg: 'assets/icons/home.svg', inactiveImg: 'assets/icons/home-bl.svg' },
+    { name: 'CATEGORIES', route: '/home', activeImg: 'assets/icons/categories.svg', inactiveImg: 'assets/icons/categories-bl.svg' },
+    { name: 'MY CART', route: '/home', activeImg: 'assets/icons/cart.svg', inactiveImg: 'assets/icons/cart-bl.svg' },
+    { name: 'PAYMENTS', route: '/home', activeImg: 'assets/icons/payments.svg', inactiveImg: 'assets/icons/payments-bl.svg' },
+    { name: 'MORE', route: '/home', activeImg: 'assets/icons/more.svg', inactiveImg: 'assets/icons/more-bl.svg' }
   ];
+
+ 
+
+  constructor(private router: Router) { }
 
   setActive(index:number){
     this.activeTab = index;
+    this.router.navigate([this.navItems[index].route])
   }
-
-  constructor() { }
 
   ngOnInit(): void {
   }
