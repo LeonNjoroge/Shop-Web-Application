76a874fbc992db26e833b89c52dfac3919e4e308
LeonNjoroge <leonmuraguri@gmail.com>
2025-02-16 14:12:01 +0300

    Update Nav Bar component

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
@@ -6,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+
+  activeTab: number | null= null;
+  
+  navItems= [
+    { name: 'HOME', activeImg: 'assets/icons/home.svg', inactiveImg: 'assets/icons/home-bl.svg' },
+    { name: 'CATEGORIES', activeImg: 'assets/icons/categories.svg', inactiveImg: 'assets/icons/categories-bl.svg' },
+    { name: 'MY CART', activeImg: 'assets/icons/cart.svg', inactiveImg: 'assets/icons/cart-bl.svg' },
+    { name: 'PAYMENTS', activeImg: 'assets/icons/payments.svg', inactiveImg: 'assets/icons/payments-bl.svg' },
+    { name: 'MORE', activeImg: 'assets/icons/more.svg', inactiveImg: 'assets/icons/more-bl.svg' }
+  ];
+
+  setActive(index:number){
+    this.activeTab = index;
+  }
 
   constructor() { }
 
